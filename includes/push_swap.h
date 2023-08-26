10231b7666ca020a1826196930772bb49bd302e2
gverissi <gscsverissimo@gmail.com>
2023-08-26 02:51:04 +0100

    added final files

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -5,30 +5,14 @@
 #include <stdio.h>
 //#include "./printf_42/ft_printf.h"
 
-typedef struct node
+typedef struct s_list
 {
-    int value;
-    struct node *next;
-} Node;
+	int				value;
+	int				index;
+	struct s_list	*next;
+}				t_list;
 
-// Function prototypes
-void sa(Node *node);
-void sb(Node *node);
-void ss(Node *stack_a, Node *stack_b);
-void pa(Node *stack_a, Node *stack_b);
-void pb(Node *stack_a, Node *stack_b);
-Node    *ra(Node *node);
-Node	*rb(Node *node);
-Node	*rr(Node **stack_a, Node **stack_b);
-Node	*rra(Node *node);
-Node	*rrb(Node *node);
-Node	*rrr(Node **stack_a, Node **stack_b);
-void print_list(Node *head);
-int recursive_length(Node *node);
-Node *insert_at_head(Node *head, int new_value);
-Node *remove_at_head(Node *head);
-Node *insert_at_tail(Node *head, int new_value);
-Node *delete_at_tail(Node *head);
-void push_swap(Node **stack_a, Node **stack_b);
+#define INT_MAX 2147483647
+#define INT_MIN -2147483648
 
 #endif /* ALLOWED_FUNCTIONS_H */
